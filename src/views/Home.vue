f8717fabb087ed5bd0fca6a3907fcbfc57f5ec72
encodedBicoding <dominicisioma000@gmail.com>
2022-07-21 13:13:35 +0100

    updates on initializing app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Michelle world.
+    Happy birthday!
   </div>
 </template>
 
